699f67fe39497bafb43776a6750ad52c7fce8f48
Towkir Ahmed <118079200+towkir93@users.noreply.github.com>
2024-03-05 19:48:09 +0100

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,1 +1,17 @@
-Main script and results (png or jpg) should be placed/saved here. See main.py/main.m for additional information. However the provided pdf is your main source of information.
+ICT Laboratory Overview - CIT Master
+
+The ANT part of the ICT laboratory held in the winter term is meant to be solved
+in groups of two in an independent fashion with minimal help from tutors. You are
+expected to solve problems on your own and organize your work as you see t.
+As a rst step the ICT lab questions must be posted on the discussion boards available
+in Stud.IP. The tutors will provide feedback after the Q/A deadline detailed in the
+timetable
+CIT masters are expected to implement a baseline digital communication system that
+matches the description in this document. Transmitter, Receiver and a simple chan-
+nel model have to be implemented using Matlab/Ovtave/Python functions to test and
+simulate the whole transmission chain.
+In the following, discusses the specic goals and requirements of this lab in
+more detail. Then, introduces the lab dates and the general timing of the
+lab over the whole winter term. explains the evaluation guidelines that will be
+used to judge if the lab has been passed successfully or not. Finally, the API denitions
+are given in Section
